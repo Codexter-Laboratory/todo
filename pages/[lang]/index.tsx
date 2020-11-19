2dd49cbe347187be81e5aafec94021fd73dea0b8
Fadi Nasr <fadi1nasr@outlook.com>
2020-11-19 19:17:11 +0200

    fix(app): Removed content of /en and /ar

--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -18,54 +18,6 @@ const Home: NextPage<Props> = () => {
 
     return (
         <div className="page-wrapper" dir={locale === 'ar' ? 'rtl' : 'ltr'}>
-            <div className={'row'}>
-                <div className="col-md-6 col-12">
-                    <Cards title="01." description="Say hello to Digital Receipts"
-                           sub_description="No need for a new hardware a new EPOS system or EPOS integration"
-                           icon="assets/ass.png"/>
-                </div>
-                <div className={'col-md-6 col-12'}>
-                </div>
-            </div>
-            <div className={`row`}>
-                <div className={"col-md-6 col-12"}>
-
-                    <CardDeck>
-                        <div className={"row"}>
-                            <div className={"col-md-6 col-12"}>
-                                <Cards title={"Hey"} description={"Im here"} sub_description={"talk to the world"}
-                                       icon={"#"}/>
-                            </div>
-                            <div className={"col-md-6 col-12"}>
-                                <Cards title={"hello"} description={"world"} sub_description={"sup"} icon={"#"}/>
-                            </div>
-                        </div>
-                    </CardDeck>
-
-                </div>
-                <div className={"col-md-6 col-12"}>
-                    <Paragraph header="Say Hello to Digital Receipts"
-                               content="Unlock the power of your in-store data by digitizing your customer's receipts"
-                               subContent="PotPay" children=""/>
-                </div>
-            </div>
-            <div className={`row`}>
-                <LogoDeck logo_1="/assets/500-logo.jpg" logo_2="/assets/flat6labs-logo.png" title={"Funded by"}/>
-                <div className="col-md-6 col-12">
-                    <div className={"row"}>
-                        <LogoDeck logo_1="assets/arabnet-logo.png" logo_2="assets/berytech-logo.png"
-                                  title={"Featured in"}/>
-                    </div>
-                </div>
-            </div>
-            <div className={`row`}>
-                <div className="col-md-6 col-12">
-                    <Contact/>
-                </div>
-                <div className="col-md-6 col-12">
-                    <Form/>
-                </div>
-            </div>
         </div>
     );
 };
